613326c22443b0bdcff5a723643caa647f1a1a8e
Not A Real Name <naji@MacBook-Pro-3.local>
2024-06-05 14:21:43 +0200

    remvign DS_store bya dding it to gitignore and updating constants.py

--- constants.py
+++ constants.py
@@ -2,9 +2,11 @@ import torch
 import numpy as np
 DTYPE = torch.float64
 ROOT_PATH = '/Users/naji/phd_codebase'
-EPSILON = 1e-6
+EPSILON = 1e-8
 BATCH_SIZE = 64
 NUMPY_DTYPE = np.float64
 # assert str(NUMPY_DTYPE).split('.')[1].split("'")[0] == str(DTYPE).split('.')[1].split("'")[0]
-RANDOM_STATE = 4442
+RANDOM_STATE = 442
 SGP_INDUCING_POINTS_SIZE = None
+LOG_LEVEL = 'WARNING'
+TUNING_RESULTS_DIR = f'{ROOT_PATH}/tuning_results'
