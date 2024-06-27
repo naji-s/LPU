0c4259a88495800be193ffdd209f89867076d879
naji <naji@example.com>
2024-06-26 22:45:06 -0400

    updating ray_utils.py to have callback for plotting the metrics and important parameters at the end of each trial iteragtion

--- utils/ray_utils.py
+++ utils/ray_utils.py
@@ -1,8 +1,10 @@
 import copy
+import os
 
-import torch
+from matplotlib import pyplot as plt
 import ray.tune
 import ray.train
+import torch
 
 class SaveBestModelCallback(ray.tune.Callback):
     def __init__(self, dataloaders_dict, best_model_path="best_model.pth"):
@@ -32,3 +34,38 @@ class SaveBestModelCallback(ray.tune.Callback):
             )
             print(f"Final Test Loss: {test_loss}")
             ray.train.report(test_loss=test_loss)
+
+class PlotMetricsCallback(ray.tune.Callback):
+    def __init__(self, allow_list=None):
+        self.metric_data = {}
+        self.trial_dir = None
+        if allow_list is None:
+            allow_list = ['time_this_iter_s', 'gamma', 'lambda', 
+                          'learning_rate', 'val_overall_loss', 'val_y_APS',
+                          'val_y_accuracy', 'val_y_auc']
+        self.allow_list = allow_list
+
+    def on_trial_start(self, iteration, trials, trial, **info):
+        self.trial_dir = trial.local_path
+        self.trial_name = trial.trial_id  # Use trial_id as the trial name
+
+    def on_trial_result(self, iteration, trials, trial, result):
+        if not self.metric_data:
+            self.metric_data = {metric_name: [] for metric_name in result.keys() if metric_name in self.allow_list}
+
+        for metric_name, metric_value in result.items():
+            if metric_name in self.allow_list:
+                self.metric_data[metric_name].append(metric_value)
+
+        for metric_name, metric_values in self.metric_data.items():
+            fig, ax = plt.subplots(figsize=(8, 4))
+            ax.plot(range(1, len(metric_values) + 1), metric_values, label=metric_name)
+            ax.set_xlabel('Epoch')
+            ax.set_ylabel(metric_name)
+            ax.legend()
+            fig.tight_layout()
+            fig.savefig(os.path.join(self.trial_dir, f"progress_{metric_name}_{self.trial_name}.png"))
+            plt.close(fig)
+
+    def on_trial_complete(self, iteration, trials, trial, **info):
+        self.metric_data = {}
